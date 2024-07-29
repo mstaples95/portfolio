11cca8e958b85a7b511efa2ee9a874977ecbc999
mstaples95 <141524898+mstaples95@users.noreply.github.com>
2024-07-29 16:22:35 -0400

    adjust-project-cards

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -5,6 +5,17 @@ import sfdbThumb from "../../assets/images/sfdb-thumb.png";
 import gggThumb from "../../assets/images/VOAoMl9.png";
 import gw2Thumb from "../../assets/images/760bdpvp-update-01.jpg";
 import gen2Thumb from "../../assets/images/paul-saveliev-gen-2.jpg";
+import reactIcon from "../../assets/icons/icons8-react-native-50.png";
+import htmlIcon from "../../assets/icons/icons8-html-50.png";
+import cssIcon from "../../assets/icons/icons8-css-50.png";
+import nodeIcon from "../../assets/icons/icons8-nodejs-50.png";
+import jsIcon from "../../assets/icons/icons8-javascript-50.png";
+import tsIcon from "../../assets/icons/icons8-typescript-50.png";
+import expressIcon from "../../assets/icons/icons8-express-js-50.png";
+import mysqlIcon from "../../assets/icons/icons8-mysql-50.png";
+import sassIcon from "../../assets/icons/icons8-sass-50.png";
+import restIcon from "../../assets/icons/icons8-rest-api-50.png";
+import wpIcon from "../../assets/icons/icons8-wordpress-50.png";
 
 
 const Featured: React.FC = () =>{
@@ -15,10 +26,10 @@ const Featured: React.FC = () =>{
                 <p className="featured__subheader">Check out some of my recent web development projects.</p>    
             </div>
             <div className="featured__project-container">
-                <ProjectCard title="GoGoGrocery" description="A meal planning web-app designed to help users figure out what they need at the grocery store." imgUrl={gggThumb}/>
-                <ProjectCard title="Street Fighter DB" description="An open-source API project for developers and fans of street Fighter." imgUrl={sfdbThumb}/>
-                <ProjectCard title="GW2 PVP Stats" description="A tool for players to see their ranked and unranked PVP progression." imgUrl={gw2Thumb}/>
-                <ProjectCard title="Pokemon Quiz" description="An app where you answer questions to find out what Pokemon matches your personality." imgUrl={gen2Thumb}/>
+                <ProjectCard title="GoGoGrocery" description="A meal planning web-app designed to help users figure out what they need at the grocery store. Choose what you want to eat each day and you'll get a nutritional breakdown for each day, as well as an itemized list of foods to make shopping easier." imgUrl={gggThumb} techIcon1={tsIcon} techIcon2={reactIcon} techIcon3={sassIcon} techIcon4={htmlIcon} techIcon5={restIcon} />
+                <ProjectCard title="Street Fighter DB" description="An open-source API project for developers and fans of street Fighter. Dive deep into detailed profiles of each fighter, from their iconic moves to their rich backstories, all at your fingertips." imgUrl={sfdbThumb} techIcon1={jsIcon} techIcon2={nodeIcon} techIcon3={expressIcon} techIcon4={mysqlIcon} techIcon5={restIcon}/>
+                <ProjectCard title="GW2 PVP Stats" description="A tool for players to see their ranked and unranked PVP progression. Using the official Guild Wars 2 API, users can retrieve data for their unique characters and see their stats on a per character or accountwide basis." imgUrl={gw2Thumb} techIcon1={jsIcon} techIcon2={htmlIcon} techIcon3={sassIcon} techIcon4={nodeIcon} techIcon5={reactIcon}/>
+                <ProjectCard title=" Gen 2 Pokemon Quiz" description="An app where you answer questions to find out what Pokemon matches your personality." imgUrl={gen2Thumb} techIcon1={jsIcon} techIcon2={htmlIcon} techIcon3={cssIcon} techIcon4={restIcon} techIcon5={wpIcon}/>
             </div>
         </div>
     )
--- src/components/Project-Card/Project-Card.tsx
+++ src/components/Project-Card/Project-Card.tsx
@@ -6,14 +6,29 @@ interface CardProps {
 title: string;
 description: string;
 imgUrl: string;
+techIcon1: string;
+techIcon2: string;
+techIcon3: string;
+techIcon4: string;
+techIcon5: string;
 }
 
-const ProjectCard: React.FC<CardProps> = ({title, description, imgUrl}) => {
+const ProjectCard: React.FC<CardProps> = ({title, description, imgUrl ,techIcon1, techIcon2, techIcon3, techIcon4, techIcon5}) => {
     return (
         <div className="project-card">
             <img className="project-card__img" src={imgUrl} alt={title}/>
             <div className="project-card__content">
                 <h3 className="project-card__heading">{title}</h3>
+                <div>
+                    <p>Made with:</p>
+                    <ul className="project-card__tech-icon-list">
+                        <img className="project-card__icon" src={techIcon1} alt="javascript"/>
+                        <img className="project-card__icon" src={techIcon2} alt=""/>
+                        <img className="project-card__icon" src={techIcon3} alt=""/>
+                        <img className="project-card__icon" src={techIcon4} alt=""/>
+                        <img className="project-card__icon" src={techIcon5} alt=""/>
+                    </ul>
+                </div>
                 <p className="project-card__text">{description}</p>
             </div>
             <div className="project-card__button-container">
